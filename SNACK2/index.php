35f2c7c30a036a04fdd6cc79430d25bae23b0361
Quintusdev <quintusdev92@gmail.com>
2023-07-03 15:03:35 +0200

    [Update] Aggiornato commenti e Titolo scheda

--- SNACK2/index.php
+++ SNACK2/index.php
@@ -27,7 +27,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Snack 3 Blocco 1 PHP</title>
+        <title>Snack 2 Blocco 1 PHP</title>
     </head>
 
     <body>
